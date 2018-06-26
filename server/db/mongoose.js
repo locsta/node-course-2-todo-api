d58735cc4566b51c2093e72b97aae555b49f3594
Marc Inizan <locstaw@gmail.com>
2018-06-26 14:15:32 +0100

    MAJ

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,10 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://locsta:qwerty123@ds117935.mlab.com:17935/nodejs-todo' || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
   mongoose
 };
+
+//process.env.MONGODB_URI || 
