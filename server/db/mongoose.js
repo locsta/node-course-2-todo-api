210866306e9454bb2a202ff31ff41d2e3cff9b50
Marc Inizan <locstaw@gmail.com>
2018-06-27 13:45:41 +0100

    UPDATE heroku server

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp' || 'mongodb://locsta:qwerty123@ds117935.mlab.com:17935/nodejs-todo');
+mongoose.connect('mongodb://locsta:qwerty123@ds117935.mlab.com:17935/nodejs-todo');
 
 module.exports = {
   mongoose
