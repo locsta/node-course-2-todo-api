984d7eb1fba08a0db06738b6ee3bc68d01ded114
Marc Inizan <locstaw@gmail.com>
2018-06-27 11:48:14 +0100

    Add DELETE /todos/:id route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,10 +1,10 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://locsta:qwerty123@ds117935.mlab.com:17935/nodejs-todo' || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://localhost:27017/TodoApp' || 'mongodb://locsta:qwerty123@ds117935.mlab.com:17935/nodejs-todo');
 
 module.exports = {
   mongoose
 };
 
-//process.env.MONGODB_URI || 
+//process.env.MONGODB_URI pour connect avec le add-on mLab d'heroku
